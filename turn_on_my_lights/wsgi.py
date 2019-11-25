7d3ef8078535d248f194047d15fcd94ae5be8480
phussaarts <pieterhussaarts@gmail.com>
2019-11-25 18:58:46 +0200

    fix

--- turn_on_my_lights/wsgi.py
+++ turn_on_my_lights/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+path = '/home/phussaarts/my_lights'
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'turn_on_my_lights.settings')
 
